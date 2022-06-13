c6a78abb2ee52da60a72eb3cb5324d12a4436bc0
Tarne <36481181+Tarnival@users.noreply.github.com>
2022-06-14 00:00:00 +0800

    Addition – Steam Workshop Amelioration 1.00
    Steam Workshop Amelioration for TamperMonkey.

---
+++ TamperMonkey/Steam_Workshop_Amelioration.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name Steam Workshop Amelioration
+// @author Deparsoul
+// @description Mitigative efforts to improve previews in Steam UGC pages.
+// @downloadURL https://raw.githubusercontent.com/Tarnival/Neat-Utan/Archive/TamperMonkey/Steam_Workshop_Amelioration.user.js
+// @grant none
+// @homepage https://userscripts.org/users/deparsoul/
+// @icon https://steamstore-a.akamaihd.net/favicon.ico
+// @license GNU General Public License Version 3
+// @match http*://steamcommunity.com/sharedfiles/filedetails/*
+// @match http*://steamcommunity.com/workshop/filedetails/*
+// @namespace http://www.tarne.cn/
+// @run-at document-end
+// @supportURL https://keylol.com/forum.php?mod=viewthread&tid=95271
+// @updateURL https://github.com/Tarnival/Neat-Utan/raw/Archive/TamperMonkey/Steam_Workshop_Amelioration.user.js
+// @version 1.00
+// ==/UserScript==
+
+if(typeof onYouTubeIframeAPIReady == 'function') {
+    onYouTubeIframeAPIReady();
+}
